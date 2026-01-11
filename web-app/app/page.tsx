4a5f798acfb29ad7a80192ad4b8059ae088cc709
Dianute <korkelt@gmail.com>
2026-01-11 22:06:35 +0200

    Update page.tsx

--- web-app/app/page.tsx
+++ web-app/app/page.tsx
@@ -167,7 +167,6 @@ export default function Home() {
         event={selectedEvent}
         theme={currentTheme}
         readOnly={!!selectedEvent}
-        readOnly={!!selectedEvent}
         feed={feedEvents}
         allEvents={events} // Pass full history for Templates
       />
